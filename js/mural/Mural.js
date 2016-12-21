20dcbb1d498bb5145d961d0f441ca9bfb679a790
Artur Diniz Adam <artur.daz@gmail.com>
2016-12-21 18:15:58 -0200

    [Salvando cartoes com localStorage] Armazenando os cartoes, serializando e deserializando

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,13 +1,21 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+    let cartoes = JSON.parse(localStorage.getItem("cartoes")).map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render()
 
     Filtro.on("filtrado", render)
+
+    function salvaCartoes(){
+        localStorage.setItem("cartoes", JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ))
+    }
 
     function adiciona(cartao){
         if(logado){
             cartoes.push(cartao)
+            salvaCartoes()
             cartao.on("mudanca.**", render)
             cartao.on("remocao", ()=>{
                 cartoes = cartoes.slice(0)
