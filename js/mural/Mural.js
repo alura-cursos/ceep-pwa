3800405a18244bdc176e6d93a0827d22dc7fc2c7
Artur Diniz Adam <artur.daz@gmail.com>
2016-12-21 18:28:20 -0200

    [Salvando cartoes com localStorage] Salvando alteracoes com listeners

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,10 +1,23 @@
 const Mural = (function(_render, Filtro){
     "use strict"
     let cartoes = JSON.parse(localStorage.getItem("cartoes")).map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao)
+    })
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
     render()
-
     Filtro.on("filtrado", render)
+
+    function preparaCartao(cartao){
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", ()=>{
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao),1)
+            salvaCartoes()
+            render()
+        })
+    }
 
     function salvaCartoes(){
         localStorage.setItem("cartoes", JSON.stringify(
@@ -17,11 +30,8 @@ const Mural = (function(_render, Filtro){
             cartoes.push(cartao)
             salvaCartoes()
             cartao.on("mudanca.**", render)
-            cartao.on("remocao", ()=>{
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao),1)
-                render()
-            })
+            preparaCartao(cartao)
+
             render()
             return true
         } else {
