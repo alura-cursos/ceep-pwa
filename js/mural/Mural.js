ffccd5dea13dbbff9e4cde6ef24ad7f12f8e7fee
alura <alura@Alura-Azul.local>
2017-01-12 05:52:37 -0200

    [Stage] Salvando Imagens com Cache Storage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -9,6 +9,15 @@ const Mural = (function(_render, Filtro){
     Filtro.on("filtrado", render)
 
     function preparaCartao(cartao){
+        const urlsImagens = Cartao.pegaImagens(cartao)
+        urlsImagens.forEach(url => {
+            fetch(url).then(resposta => {
+                caches.open("ceep-imagens").then(cache => {
+                    cache.put(url, resposta)
+                })
+            })
+        })
+
         cartao.on("mudanca.**", salvaCartoes)
         cartao.on("remocao", ()=>{
             cartoes = cartoes.slice(0)
